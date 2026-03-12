3fe4eb484ecf5ab6765b98e417169fabcea0ea41
Ivan <ivanqqq@ukr.net>
2026-03-12 20:42:09 +0100

    fix comments

--- task.sql
+++ task.sql
@@ -28,7 +28,7 @@ CREATE TABLE Logs (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Timestamp TIMESTAMP,
     Message VARCHAR(100)
-) ENGINE=InnoDB;
+) ENGINE=Blackhole;
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
     ID INT NOT NULL,
